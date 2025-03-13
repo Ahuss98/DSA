6e114d3bc8e9ca87509543f5f189ab3adebebf60
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-03-13 17:25:53 +0000

    linear and binary

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,59 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] nums = {5,7,9,11,13,15};
+        int target = 9;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int result = linearSearch(nums,target);
+        int result1 = binarySearch(nums,target);
+
+        if(result != -1){
+            System.out.println("element found at index: " +result);
+        } else {
+            System.out.println("element not found");
         }
+
+
+        if(result1 != -1){
+            System.out.println("element found at index: " + result1);
+        } else {
+            System.out.println("element not found");
+        }
+
     }
+    public static int linearSearch(int[] nums, int target){
+        int steps = 0;
+        for(int i = 0; i < nums.length;i++){
+            steps++;
+            if(nums[i] == target){
+                System.out.println("linear was "+steps+ " steps");
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int binarySearch(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+        int steps = 0;
+
+        while(left <= right){
+        steps++;
+        int middleIndex =  (right+left)/2;
+
+            if(nums[middleIndex] == target){
+                System.out.println("binary was " + steps+" steps");
+                return middleIndex;
+
+            } else if(target < nums[middleIndex]){
+                right = middleIndex - 1;
+
+            } else {
+                left = middleIndex + 1;
+
+            }
+        }
+        return -1;
+    }
+
 }
