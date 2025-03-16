d7a3b7c7d043104dc16f19262373a3162dd349fc
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-03-16 04:15:21 +0000

    practice arrays

---
+++ src/main/java/org/dataStructresPractice/TheArrays.java
@@ -0,0 +1,13 @@
+package org.dataStructresPractice;
+
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String[] colours = new String[5];
+        colours[0] = "purple";
+        colours[1] = "blue";
+
+        System.out.println(Arrays.toString(colours));
+    }
+}
