4c0d022b7e4a729afa3830b0aa7b22497e807550
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-03-20 21:18:21 +0000

    bibnary

--- src/main/java/org/example/buvlesorting.java
+++ src/main/java/org/example/buvlesorting.java
@@ -5,38 +5,37 @@ import java.util.Arrays;
 public class buvlesorting {
     public static void main(String[] args) {
         int[] numbers = {2,6,4,9,1,};
-        System.out.println(buble(numbers));
-        System.out.println(binary(numbers,1));
+        System.out.println(Arrays.toString(buble(numbers)));
+        System.out.println(binary(buble(numbers),9));
 
     }
         public static int[] buble(int[] array){
-        for(int i = 0; i < array.length ; i++){
-            for(int j = 0; j < array.length - i - 1; j++) {
+        for(int i = 0; i< array.length;i++){
+            for(int j = 0; j<array.length-i-1; j++){
                 if(array[j] > array[j+1]){
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
                 }
             }
+
         }
-        return array;
+            return array;
         }
 
         public static boolean binary(int[] array, int myNumb){
             int l = 0;
             int r = array.length -1;
-
             while(l<=r){
-                int mid = l + ((array.length)/2 - 1);
-                if(mid == myNumb){
+                int m = l+ (r-l)/2;
+                if(array[m] == myNumb){
                     return true;
-                } else if(mid > myNumb){
-                    l = mid+1;
-                } else if( mid < myNumb){
-                    r = mid -1;
+                } else if (array[m] > myNumb){
+                    r = m-1;
+                } else {
+                    l = m + 1;
                 }
             }
-
             return false;
         }
 }
